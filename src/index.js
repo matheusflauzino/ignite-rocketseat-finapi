c0b958c8cd7cb081ddb62db1570cd13130690e6f
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2022-05-12 23:45:31 -0300

    feat: stantment and deposit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found!" })
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
 
@@ -23,6 +37,28 @@ app.post('/account', (request, response) => {
         id,
         statement: []
     });
+
+    return response.status(201).send();
+
+})
+
+app.get("/stantment", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
+})
+
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const stantmentOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(stantmentOperation);
 
     return response.status(201).send();
 
