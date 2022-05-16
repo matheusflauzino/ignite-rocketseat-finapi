94c956fde0ef4eabf7e86f06da242c2b125c0c55
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2022-05-15 22:41:49 -0300

    feat: adiciona endpoint de saque

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
 
@@ -62,6 +74,27 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
 
     return response.status(201).send();
 
+});
+
+app.post('/withdraw', verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: 'Insufficient funds!' })
+    }
+
+    const stantmentOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(stantmentOperation);
+
+    return response.status(201).send();
 })
 
 app.listen(3333);
